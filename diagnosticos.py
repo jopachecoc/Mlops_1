355e084327ef0ae6e0c8aff4cff7ab328e6eeca0
jonathan <32146205+jopachecoc@users.noreply.github.com>
2025-05-16 17:26:05 -0500

    Update diagnosticos.py

--- diagnosticos.py
+++ diagnosticos.py
@@ -30,4 +30,4 @@ def enfermedad1(Presion_sistolica, Presion_diastolica, Pulso, Temperatura):
         categoria = 'ENFERMEDAD CRÓNICA'
 
     return categoria
-## va a correr con exito
+## va a correr con exito de una vez
