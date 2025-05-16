d87ea498990130183dac40ed941549a45ef907e2
jonathan <32146205+jopachecoc@users.noreply.github.com>
2025-05-16 15:32:34 -0500

    Create test_funcion.py

---
+++ test_funcion.py
@@ -0,0 +1,7 @@
+from diagnosticos import enfermedad1
+def test_enfermedad():
+  assert test_enfermedad(110,79,90,37) == 'NO ENFERMO'
+  assert test_enfermedad(110,79,90,38) == 'ENFERMEDAD LEVE'
+  assert test_enfermedad(110,79,101,37) == 'ENFERMEDAD LEVE'
+  assert test_enfermedad(121,79,90,37) == 'ENFERMEDAD CRÓNICA'
+  
