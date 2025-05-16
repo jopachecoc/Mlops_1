8fa26523abcd76c384a414f7133d048d14c242f2
jonathan <32146205+jopachecoc@users.noreply.github.com>
2025-05-16 17:23:27 -0500

    Update test_funcion.py

--- test_funcion.py
+++ test_funcion.py
@@ -1,7 +1,7 @@
 from diagnosticos import enfermedad1
 def test_enfermedad():
-  assert test_enfermedad(110,79,90,37) == 'NO ENFERMO'
-  assert test_enfermedad(110,79,90,38) == 'ENFERMEDAD LEVE'
-#  assert test_enfermedad(110,79,101,37) == 'ENFERMEDAD AGUDAAAAAA'
-  assert test_enfermedad(121,79,90,37) == 'ENFERMEDAD CRÓNICA'
+  assert enfermedad1(110,79,90,37) == 'NO ENFERMO'
+  assert enfermedad1(110,79,90,38) == 'ENFERMEDAD LEVE'
+#  assert enfermedad1(110,79,101,37) == 'ENFERMEDAD AGUDAAAAAA'
+  assert enfermedad1(121,79,90,37) == 'ENFERMEDAD CRÓNICA'
   
